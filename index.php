57caf71772f9d8899f13b903f6a7dfdf5ffca30a
Ganeshpandi <you@example.com>
2016-09-16 15:37:55 +0530

    update index.php

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
-
-down vote
-try this (if your server is Linux):
-
-$command="/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'";
-$localIP = exec ($command);
-echo $localIP;
-//echo "Hi Hello 121outsource";
+function getLocalIp()
+{ return gethostbyname(trim(`hostname`)); }
 ?>
+
