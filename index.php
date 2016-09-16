2c5c138bbc58b1f51f982955ba8d78b610c34990
Ganeshpandi <you@example.com>
2016-09-16 15:30:32 +0530

    update index.php

--- index.php
+++ index.php
@@ -6,5 +6,5 @@ try this (if your server is Linux):
 $command="/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'";
 $localIP = exec ($command);
 echo $localIP;
-echo "Hi Hello 121outsource""
+echo "Hi Hello 121outsource";
 ?>
